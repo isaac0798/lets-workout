6e945dbfd133f9756d9c3aceac32b7235b3812b1
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2020-12-21 15:42:42 +0000

    Adding google auth

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cookieParser = require('cookie-parser');
 const port = 3000
 const path = require('path')
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/api/account/google"
+  },
+  (accessToken, refreshToken, profile, done) => {
+    return done(null, profile)
+  }
+));
+
+app.get('/auth', passport.authenticate('google', 
+  { 
+    scope:  ["profile"], 
+    prompt: 'select_account'
+  }));
+app.get('/auth/error', (req, res) => res.send('Unknown Error'))
+app.get('/api/account/google', passport.authenticate('google', { failureRedirect: '/auth/error' }),
+  function(req, res) {
+    res.cookie("lets_workout_user", req.user);
+    res.redirect('/');
+  }
+);
 
 app.use(express.static(__dirname+'/public'))
 
 app.get('/', (req, res) => {
+    if (!req.cookies.lets_workout_user) {
+      res.redirect('/login')
+    }
+  
     res.sendFile(path.join(__dirname+'/public/html/index.html'));
+})
+
+app.get('/welcome', (req, res) => {
+    res.sendFile(path.join(__dirname+'/public/html/welcome.html'));
+})
+
+app.get('/logout', (req, res) => {
+  res.clearCookie("lets_workout_user");
+  req.logout();
+  res.redirect('/welcome')
 })
 
 app.listen(port, () => {
