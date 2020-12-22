5e715c190d093e567df490e74400705419f76f9d
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2020-12-22 15:16:52 +0000

    checking whether user exists before saving

--- backend/sql/repository/saveUser.js
+++ backend/sql/repository/saveUser.js
@@ -1,6 +1,6 @@
 const translateGoogleProfile = require('../../translators/translateGoogleProfile.js');
 
-const saveUser = (profile, connection) => {
+const saveUser = async (profile, connection) => {
   const translatedProfile = translateGoogleProfile(profile); 
   const values = [
     translatedProfile.display_name,
@@ -10,11 +10,25 @@ const saveUser = (profile, connection) => {
     translatedProfile.profile_pic_url
   ];
   const sql = "INSERT INTO workout_user (display_name, first_name, last_name, email, profile_pic_url) VALUES (?)";
-
-  connection.query(sql, [values],function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
+  checkIfUserExists(translatedProfile.email, connection).then((results) => {
+    if (results.length === 0) {
+      connection.query(sql, [values],function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+      });
+    }
   });
+
 };
+
+const checkIfUserExists = (userEmail, connection) => {
+  return new Promise(function(resolve, reject) {
+    const sql = `SELECT * from workout_user WHERE email='${userEmail}'`;
+    connection.query(sql, function(err, result) {
+      if (err) reject(new Error(err));
+      resolve(result);
+    });
+  });
+}
 
 module.exports = saveUser;
