79a777ee875f77cb08f1b17e2aa101e1b99fa07f
isaac0798 <isaac0798@gmail.com>
2021-01-01 18:00:33 +0000

    if user weight entry is logged for the day post request updates it

--- backend/sql/repository/UserWeight/insertWeight.js
+++ backend/sql/repository/UserWeight/insertWeight.js
@@ -1,7 +1,9 @@
+const dateFormat = require('dateformat');
 const {getConnection} = require('../../connect.js'); 
 const {getUserId} = require('../../../verifyUser.js');
 
 const insertWeight = async (user, reqParams) => {
+  const userEmail = user.emails[0].value;
   if (!verifyWeightParams(reqParams)) {
     return "Invalid parameters";
   }
@@ -10,8 +12,17 @@ const insertWeight = async (user, reqParams) => {
     return "Invalid user";
   }
 
-  const userId = await getUserId(user.emails[0].value);
+  const userId = await getUserId(userEmail);
   const connection = getConnection();
+
+  const dailyWeight = await checkForDailyWeight(userEmail, userId, connection);
+
+  if (dailyWeight) {
+    updateDailyWeight(reqParams, userId, connection);  
+
+    return true;
+  }
+
   const sql = "INSERT INTO user_weight (user_id, weight, date, unit) VALUES (?)";
   connection.query(sql, [[
     userId,
@@ -24,6 +35,34 @@ const insertWeight = async (user, reqParams) => {
   });
 
   return true;
+}
+
+const updateDailyWeight = ({unit, date, weight}, userId, connection) => {
+  return new Promise(function(resolve, reject) {
+    const sql = `UPDATE user_weight SET unit='${unit}', weight=${weight} WHERE date='${date}' AND user_id=${userId}`;
+    connection.query(sql, function(err, result) {
+      if (err) reject(new Error(err));
+      console.log(result);
+      resolve(result);
+    });
+  });
+}
+
+const checkForDailyWeight = (userEmail, userId, connection) => {
+  return new Promise(function(resolve, reject) {
+    const todayDate = getTodaysDate();
+    const sql = `SELECT * from user_weight WHERE date='${todayDate}' AND user_id=${userId}`;
+    connection.query(sql, function(err, result) {
+      if (err) reject(new Error(err));
+      resolve(result.length);
+    });
+  });  
+}
+
+const getTodaysDate = () => {
+  const today = new Date();
+
+  return dateFormat(today, "yyyy-mm-dd");
 }
 
 
